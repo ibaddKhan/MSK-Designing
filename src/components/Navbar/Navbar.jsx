875170ecd1b9ddc79884830f4c6de90887649b1c
ibaddkhan <ibadurrehman718@gmail.com>
2024-11-18 01:59:53 +0500

    updated

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import Hamburger from "hamburger-react";
+import Logo from "../../assets/logo.png";
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="absolute top-0 left-0 flex items-center w-full z-10 mt-10 bg-opacity-50 text-white py-4 px-4">
+      <div className="flex justify-start">
+        <img src={Logo} className=" w-32" alt="Logo" />
+      </div>
+      <div className="flex justify-between items-center w-full md:w-auto space-x-7 mx-auto">
+        {/* Hamburger Menu on Mobile (Aligned to the right) */}
+        <div className="block md:hidden ml-auto">
+          <Hamburger toggled={isOpen} toggle={setIsOpen} color="#fff" />
+        </div>
+
+        {/* Desktop Menu (Hidden on Mobile, Visible on Larger Screens) */}
+        <div className="hidden md:flex space-x-7 ml-auto">
+          <a className="nav-link">HOME</a>
+          <a className="nav-link">ARTICLES</a>
+          <a className="nav-link">PORTFOLIO</a>
+          <a className="nav-link">ABOUT</a>
+          <a className="nav-link">CONTACT</a>
+        </div>
+      </div>
+
+      <div
+        className={`md:hidden ${
+          isOpen ? "block" : "hidden"
+        } absolute top-16 left-0 w-full bg-black text-white py-4 px-4 space-y-4 overflow-x-hidden`}
+      >
+        <a className="nav-link">HOME</a>
+        <a className="nav-link">ARTICLES</a>
+        <a className="nav-link">PORTFOLIO</a>
+        <a className="nav-link">ABOUT</a>
+        <a className="nav-link">CONTACT</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,41 +1,13 @@
 import React, { useState } from "react";
-import Hamburger from "hamburger-react";
+import Navbar from "../../components/Navbar/Navbar";
 import bg_fig from "../../assets/hero_bg.png";
-import Logo from "../../assets/logo.png";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
       <div>
-        {/* Navbar */}
-        <div className="absolute top-0 left-0 flex justify-between items-center w-full z-10 mt-10 bg-opacity-50 text-white py-4 px-4">
-          <div>
-            <img src={Logo} className="w-24" />
-          </div>
-          <div className="flex justify-between items-center w-full md:w-auto space-x-7 mx-auto">
-            {/* Hamburger Menu */}
-            <div className="block md:hidden">
-              <Hamburger toggled={isOpen} toggle={setIsOpen} color="#fff" />
-            </div>
-
-            {/* Desktop Menu */}
-            <div
-              className={`md:flex space-x-7 ${
-                isOpen ? "block" : "hidden"
-              } md:block`}
-            >
-              <a className="nav-link">HOME</a>
-              <a className="nav-link">ARTICLES</a>
-              <a className="nav-link">PORTFOLIO</a>
-              <a className="nav-link">ABOUT</a>
-              <a className="nav-link">CONTACT</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="bg-black w-screen h-[700px]">
+        <Navbar />
+        <div className="bg-black  h-[700px]">
           <img
             src={bg_fig}
             alt="bg_cover_img"
